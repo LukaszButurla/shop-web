29f69e73433d47689b56b740d1ef0ccf37276354
DESKTOP-HF35O0S\lukasz <lukasz.buturla.25@gmail.com>
2023-03-12 20:58:04 +0100

    Create add user function

--- website/__init__.py
+++ website/__init__.py
@@ -1,12 +1,23 @@
 from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
 from .views import views
-from .auth import auth
+
+db = SQLAlchemy()
+DB_NAME = "database.db"
 
 def create_app():
     app = Flask(__name__)
     app.config["SECRET_KEY"] = "app123"
+    app.config["SQLALCHEMY_DATABASE_URI"] = f"sqlite:///{DB_NAME}"
+    db.init_app(app)
+    
+    from .models import User
+    from .auth import auth
     
     app.register_blueprint(views, url_prefix="/")
     app.register_blueprint(auth, url_prefix= "/")
+    
+    with app.app_context():
+        db.create_all()
     
     return app
